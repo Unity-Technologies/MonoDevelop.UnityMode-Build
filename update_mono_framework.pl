7e8e78ea5ef684b01fac90c628683e78d6640aa8
Lucas Meijer <lucas@unity3d.com>
2014-09-04 11:02:16 +0200

    WIP commit

--- update_mono_framework.pl
+++ update_mono_framework.pl
@@ -11,8 +11,8 @@ system("mkdir $mf/Versions");
 my $current = "$mf/Versions/Current";
 system("mkdir $current");
 system("cp -r /Library/Frameworks/Mono.framework/Versions/Current/* $current");
-rmtree("$current/lib/mono/gac");
-rmtree("$current/lib/mono/xbuild-frameworks");
+#rmtree("$current/lib/mono/gac");
+#rmtree("$current/lib/mono/xbuild-frameworks");
 rmtree("$current/lib/mono/monodroid");
 rmtree("$current/lib/mono/monotouch");
 rmtree("$current/lib/ironruby");
@@ -26,17 +26,47 @@ rmtree("$current/share/autoconf");
 rmtree("$current/share/automake-1.13");
 rmtree("$current/share/libtool");
 rmtree("$current/share/man");
-rmtree("$current/etc/xml");
+#rmtree("$current/etc/xml");
+rmtree("$current/lib/mono/xbuild");
+rmtree("$current/lib/mono/Microsoft SDKs");
+rmtree("$current/lib/mono/Microsoft F#");
 
-system("rm $current/lib/*.a");
-system("rm -r $current/lib/*.dSYM");
-system("rm -r $current/lib/*llvm.dylib");
-system("rm -r $current/lib/*llvm.0.dylib");
-system("rm -r $current/bin/*.dSYM");
+#system("rm $current/lib/*.a");
+#system("rm -r $current/lib/*.dSYM");
+#system("rm -r $current/lib/*llvm.dylib");
+#system("rm -r $current/lib/*llvm.0.dylib");
+#system("rm -r $current/bin/*.dSYM");
 system("rm -r $current/lib/mono/4.5/FSharp.*");
 system("rm -r $current/lib/mono/4.0/FSharp.*");
 system("rm -r $current/lib/mono/portable-*");
-system("rm -r $current/bin/opt");
-system("rm -r $current/bin/llc");
-system("rm -r $current/lib/libLTO.dylib");
-system("rm -r $current/bin/pedump");
+#system("rm -r $current/lib/libLTO.dylib");
+#system("find $current/bin ! -name mono -type f -delete");
+#system("rm $current/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache");
+#system("rm $current/lib/gtk-2.0/2.10.0/immodules.cache");
+
+mkpath("$current/etc/pango");
+my $filename = "$current/etc/pango/pangorc";
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+print $fh "[Pango]\n";
+print $fh "ModuleFiles = /Library/Frameworks/Mono.framework/Versions/3.6.0/etc/pango/pango.modules\n";
+close $fh;
+
+chdir($current);
+my @array = `grep -RIl /Library/Frameworks/Mono *`;
+
+my $relocatescript = "#!/bin/sh\n";
+$relocatescript .= "DIR=\"\$( cd \"\$( dirname \"\${BASH_SOURCE[0]}\" )\" && pwd )\"\n";
+$relocatescript .= "cd \$DIR\n";
+foreach $line (@array)
+{
+	chomp($line);
+	system("cp $line $line.in");
+	$relocatescript .= "sed \"s,/Library/Frameworks/Mono.framework/Versions/3.6.0,\$DIR,g\" \"$line.in\" > \"$line\"\n";
+}
+
+my $filename = "$current/relocate_mono.sh";
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+print $fh $relocatescript;
+close $fh;
+
+print $relocatescript;
